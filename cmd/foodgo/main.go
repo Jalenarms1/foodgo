44b71660e5b96fa84e9eb07715e2e4cd00171774
Jalenarms1 <Jalenarms@outlook.com>
2025-03-01 16:24:37 -0700

    adding auth logic and docker file for deployment testing

--- cmd/foodgo/main.go
+++ cmd/foodgo/main.go
@@ -1,40 +1,35 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"os"
 
-	"github.com/Jalenarms1/foodgo/internal/db"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func init() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal(err)
-	}
+	// if err := godotenv.Load(); err != nil {
+	// 	log.Fatal(err)
+	// }
 
 }
 
 func main() {
-	dbUrl := os.Getenv("DB_URL")
+	// dbUrl := os.Getenv("DB_URL")
 
-	config, err := pgxpool.ParseConfig(dbUrl)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	// config, err := pgxpool.ParseConfig(dbUrl)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 
-	config.MaxConns = 5
+	// config.MaxConns = 5
 
-	db.Pool, err = pgxpool.NewWithConfig(context.Background(), config)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Pool.Close()
+	// db.Pool, err = pgxpool.NewWithConfig(context.Background(), config)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer db.Pool.Close()
 
 	addr := os.Getenv("API_ADDR")
 
