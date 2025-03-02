ac454cc6147a07a8e87042e1ee69d5172e405b93
Jalenarms1 <Jalenarms@outlook.com>
2025-03-02 00:40:02 -0700

    adding db logic

--- cmd/foodgo/main.go
+++ cmd/foodgo/main.go
@@ -5,14 +5,13 @@ import (
 	"os"
 
 	"github.com/Jalenarms1/foodgo/internal/db"
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func init() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal(err)
-	}
+	// if err := godotenv.Load(); err != nil {
+	// 	log.Fatal(err)
+	// }
 }
 
 func main() {
